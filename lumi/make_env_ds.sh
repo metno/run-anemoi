5dde855ba21f38b2ee58d982ea36232cbcbb9f7c
paulina-t <25662348+paulina-t@users.noreply.github.com>
2025-08-19 12:29:00 +0200

    Update make_env_ds.sh

--- lumi/make_env_ds.sh
+++ lumi/make_env_ds.sh
@@ -3,7 +3,7 @@
 cd $(pwd -P)
 
 # Make files executable in the container (might not be needed)
-chmod 770 env_setup_ds_new_container.sh
+chmod 770 env_setup_ds.sh
 
 PROJECT_DIR=/pfs/lustrep2/scratch/project_465001893
 CONTAINER=$PROJECT_DIR/anemoi/containers/pytorch-2.7.0-rocm-6.2.4-py-3.12.9-v2.0.sif
