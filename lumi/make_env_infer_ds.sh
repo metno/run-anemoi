295655b724cbe41d4f47cf77b3bf0e93c45a3d90
paulina-t <25662348+paulina-t@users.noreply.github.com>
2025-09-23 14:03:29 +0200

    Create make_env_infer_ds.sh

---
+++ lumi/make_env_infer_ds.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $(pwd -P)
+
+# Make files executable in the container (might not be needed)
+chmod 770 env_setup_infer_ds.sh
+
+PROJECT_DIR=/pfs/lustrep2/scratch/project_465001893
+CONTAINER=$PROJECT_DIR/anemoi/containers/pytorch-2.7.0-rocm-6.2.4-py-3.12.9-v2.0.sif
+
+# Clone and pip install anemoi repos from the container
+singularity exec -B /pfs:/pfs $CONTAINER $(pwd -P)/env_setup_infer_ds.sh
+
